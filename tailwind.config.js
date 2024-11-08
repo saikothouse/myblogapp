bdc84b8f3d06a1a18cf46e7fad6c044ee64e4d8f
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-08 18:05:51 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -74,6 +74,7 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
+    require('@tailwindcss/line-clamp'),
     require("tailwind-bootstrap-grid")({ generateContainer: false }),
   ],
 };
