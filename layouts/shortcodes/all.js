fd7be120e6a8f0004d099e303d7fb86834d57c4c
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-08 19:20:52 +0600

    Update all.js

--- layouts/shortcodes/all.js
+++ layouts/shortcodes/all.js
@@ -1,7 +1,32 @@
-import Button from "./Button";
+import Audio from './Audio';
+import Video from './Video';
+import YouTube from './YouTube';
+import Gist from './Gist';
+import DownloadButton from './DownloadButton';
+import CodeBlock from './CodeBlock';
+import Tooltip from './Tooltip';
+import Alert from './Alert';
+import Quote from './Quote';
+import Card from './Card';
+import List from './List';
+import ProgressBar from './ProgressBar';
+import Notification from './Notification';
 
+// Exporting all shortcodes
 const shortcodes = {
-  Button,
+  Audio,
+  Video,
+  YouTube,
+  Gist,
+  DownloadButton,
+  CodeBlock,
+  Tooltip,
+  Alert,
+  Quote,
+  Card,
+  List,
+  ProgressBar,
+  Notification,
 };
 
 export default shortcodes;
